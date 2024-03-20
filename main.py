a095f334146969929898e5b3cd9e16893c62c686
Quentin Couland <Coulis1990@gmail.com>
2024-03-20 16:51:10 +0100

    :art: Refactoring

--- main.py
+++ main.py
@@ -11,31 +11,33 @@ def parserHandling():
     parser.add_argument("-w", "--white_border_size", dest="white_border_size", help="White border size", default=50)
     return parser.parse_args()
 
-def main(input_path, output_path, border_size = 50, add_black=False):
-    im = Image.open(input_path)
+def processOnePicture(file_input_path, output_path, white_border_size, add_black_border):
+    im = Image.open(file_input_path)
     original_size = im.size
-    new_size = (original_size[0] + border_size, original_size[1] + border_size)
+    new_size = (original_size[0] + white_border_size, original_size[1] + white_border_size)
     new_im = Image.new("RGB", new_size, (255, 255, 255))
-    new_pos = (int(border_size / 2), int(border_size / 2))
+    new_pos = (int(white_border_size / 2), int(white_border_size / 2))
     new_im.paste(im, new_pos)
 
-    if add_black:
+    if add_black_border:
         new_size_black = (new_size[0], new_size[1] + 1000)
         new_im_with_black = Image.new("RGB", new_size_black)
         new_pos = (0, 500)
         new_im_with_black.paste(new_im, new_pos)
-        new_im_with_black.save(pathlib.Path(output_path, input_path.stem + "_border_" + str(border_size) + "_black" + input_path.suffix))
+        new_im_with_black.save(pathlib.Path(output_path, file_input_path.stem + "_border_" + str(white_border_size) + "_black" + file_input_path.suffix))
 
 
-    new_im.save(pathlib.Path(output_path, input_path.stem + "_border_" + str(border_size) + input_path.suffix))
+    new_im.save(pathlib.Path(output_path, file_input_path.stem + "_border_" + str(white_border_size) + file_input_path.suffix))
 
-if __name__ == "__main__":
-    args = parserHandling()
-
+def main(args, add_black_border=False):
     output_path = pathlib.Path(args.output_path)
 
     filenames = [path for path in os.listdir(args.input_path) if path[-3:] != "txt"]
 
     for file in filenames:
         file_input_path = pathlib.Path(args.input_path, file)
-        main(file_input_path, args.output_path, args.white_border_size, False)
+        processOnePicture(file_input_path, output_path, args.white_border_size, add_black_border)
+
+if __name__ == "__main__":
+    args = parserHandling()
+    main(args)
